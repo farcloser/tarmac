49730bdaca8c4d2c445f8389077f61b5b428111f
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-08-06 23:10:26 -0700

    Make the test able to control destination
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+readonly expected="$1"
+
 echo "Testing unattended forced install"
 
 POSH_CASK="" POSH_TMP="" POSH_BIN="" POSH_TOKEN="" TARMAC_FORCE_INSTALL=true ./init
+
 # shellcheck source=/dev/null
 . ~/.profile
-[ "$(command -v brew)" == "/Users/travis/Applications/bin/homebrew/bin/brew" ] || exit 1
+
+if [ "$(command -v brew)" != "$expected/Applications/bin/homebrew/bin/brew" ]; then
+  echo "brew installation failed - output of command -v brew: $(command -v brew)"
+  exit 1
+fi
 
 echo "Homebrew installation Successful"
 
