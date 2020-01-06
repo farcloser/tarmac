af4f6d4affc6a7f2a12f8ccda8fbd02362f027f8
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-01-05 23:09:10 -0800

    Cosmetic
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 # Linting
 . tooling/helpers.sh
 lint::shell init ./*.sh ./*/*.sh
->&2 printf "Linting Successful"
+>&2 printf "Linting successful\n"
 
 # Installation test
 expected="$*"
@@ -17,8 +17,8 @@ POSH_CASK="" POSH_TMP="" POSH_BIN="" POSH_TOKEN="" TARMAC_FORCE_INSTALL=true ./i
 . ~/.profile
 
 if [ "$(command -v brew)" != "$expected/Applications/bin/homebrew/bin/brew" ]; then
-  >&2 printf "brew installation failed - expected: %s - command -v brew: %s" "$expected" "$(command -v brew)"
+  >&2 printf "brew installation failed - expected: %s - command -v brew: %s\n" "$expected" "$(command -v brew)"
   exit 1
 fi
 
->&2 printf "Homebrew installation Successful"
+>&2 printf "Homebrew installation successful\n"
